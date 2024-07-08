f9a13c84a799106b43e8f4a773c7501a71bd96a6
lukaaduashvili <lukaaduashvili@gmail.com>
2024-07-08 12:48:30 +0400

    Fix opcodes

--- Websocket/Opcodes.go
+++ Websocket/Opcodes.go
@@ -3,9 +3,9 @@ package Websocket
 type OpCode int
 
 const (
-	Continuation OpCode = 1
-	Text         OpCode = 2
-	Binary       OpCode = 3
+	Continuation OpCode = 0
+	Text         OpCode = 1
+	Binary       OpCode = 2
 	Close        OpCode = 8
 	Ping         OpCode = 9
 	Pong         OpCode = 10
